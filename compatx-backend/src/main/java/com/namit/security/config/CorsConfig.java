8161477e6d2eb1585c56980e24cd3d306925d959
Namit Rohekar <namit.rohekar.dev@gmail.com>
2025-12-18 18:36:07 +0530

    fix: allow vercel cors

--- compatx-backend/src/main/java/com/namit/security/config/CorsConfig.java
+++ compatx-backend/src/main/java/com/namit/security/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://compat-x.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
